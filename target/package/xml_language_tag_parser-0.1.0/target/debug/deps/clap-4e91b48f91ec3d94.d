85be3530ab0c6d36430f1e3389f9f560830e9851
Bohdana Prokopchuk <b.prokopchuk@ukma.edu.ua>
2024-11-07 04:07:09 +0200

    Initial commit

---
+++ target/package/xml_language_tag_parser-0.1.0/target/debug/deps/clap-4e91b48f91ec3d94.d
@@ -0,0 +1,9 @@
+C:\Users\Dana\rust-learning-path\xml_language_tag_parser\target\package\xml_language_tag_parser-0.1.0\target\debug\deps\libclap-4e91b48f91ec3d94.rmeta: C:\Users\Dana\.cargo\registry\src\index.crates.io-6f17d22bba15001f\clap-4.5.20\src/lib.rs C:\Users\Dana\.cargo\registry\src\index.crates.io-6f17d22bba15001f\clap-4.5.20\src\../examples/demo.rs C:\Users\Dana\.cargo\registry\src\index.crates.io-6f17d22bba15001f\clap-4.5.20\src\../examples/demo.md
+
+C:\Users\Dana\rust-learning-path\xml_language_tag_parser\target\package\xml_language_tag_parser-0.1.0\target\debug\deps\libclap-4e91b48f91ec3d94.rlib: C:\Users\Dana\.cargo\registry\src\index.crates.io-6f17d22bba15001f\clap-4.5.20\src/lib.rs C:\Users\Dana\.cargo\registry\src\index.crates.io-6f17d22bba15001f\clap-4.5.20\src\../examples/demo.rs C:\Users\Dana\.cargo\registry\src\index.crates.io-6f17d22bba15001f\clap-4.5.20\src\../examples/demo.md
+
+C:\Users\Dana\rust-learning-path\xml_language_tag_parser\target\package\xml_language_tag_parser-0.1.0\target\debug\deps\clap-4e91b48f91ec3d94.d: C:\Users\Dana\.cargo\registry\src\index.crates.io-6f17d22bba15001f\clap-4.5.20\src/lib.rs C:\Users\Dana\.cargo\registry\src\index.crates.io-6f17d22bba15001f\clap-4.5.20\src\../examples/demo.rs C:\Users\Dana\.cargo\registry\src\index.crates.io-6f17d22bba15001f\clap-4.5.20\src\../examples/demo.md
+
+C:\Users\Dana\.cargo\registry\src\index.crates.io-6f17d22bba15001f\clap-4.5.20\src/lib.rs:
+C:\Users\Dana\.cargo\registry\src\index.crates.io-6f17d22bba15001f\clap-4.5.20\src\../examples/demo.rs:
+C:\Users\Dana\.cargo\registry\src\index.crates.io-6f17d22bba15001f\clap-4.5.20\src\../examples/demo.md:
---
+++ target/package/xml_language_tag_parser-0.1.0/target/debug/deps/thiserror-2594bb719e8891f5.d
@@ -0,0 +1,9 @@
+C:\Users\Dana\rust-learning-path\xml_language_tag_parser\target\package\xml_language_tag_parser-0.1.0\target\debug\deps\libthiserror-2594bb719e8891f5.rmeta: C:\Users\Dana\.cargo\registry\src\index.crates.io-6f17d22bba15001f\thiserror-1.0.68\src/lib.rs C:\Users\Dana\.cargo\registry\src\index.crates.io-6f17d22bba15001f\thiserror-1.0.68\src\aserror.rs C:\Users\Dana\.cargo\registry\src\index.crates.io-6f17d22bba15001f\thiserror-1.0.68\src\display.rs
+
+C:\Users\Dana\rust-learning-path\xml_language_tag_parser\target\package\xml_language_tag_parser-0.1.0\target\debug\deps\libthiserror-2594bb719e8891f5.rlib: C:\Users\Dana\.cargo\registry\src\index.crates.io-6f17d22bba15001f\thiserror-1.0.68\src/lib.rs C:\Users\Dana\.cargo\registry\src\index.crates.io-6f17d22bba15001f\thiserror-1.0.68\src\aserror.rs C:\Users\Dana\.cargo\registry\src\index.crates.io-6f17d22bba15001f\thiserror-1.0.68\src\display.rs
+
+C:\Users\Dana\rust-learning-path\xml_language_tag_parser\target\package\xml_language_tag_parser-0.1.0\target\debug\deps\thiserror-2594bb719e8891f5.d: C:\Users\Dana\.cargo\registry\src\index.crates.io-6f17d22bba15001f\thiserror-1.0.68\src/lib.rs C:\Users\Dana\.cargo\registry\src\index.crates.io-6f17d22bba15001f\thiserror-1.0.68\src\aserror.rs C:\Users\Dana\.cargo\registry\src\index.crates.io-6f17d22bba15001f\thiserror-1.0.68\src\display.rs
+
+C:\Users\Dana\.cargo\registry\src\index.crates.io-6f17d22bba15001f\thiserror-1.0.68\src/lib.rs:
+C:\Users\Dana\.cargo\registry\src\index.crates.io-6f17d22bba15001f\thiserror-1.0.68\src\aserror.rs:
+C:\Users\Dana\.cargo\registry\src\index.crates.io-6f17d22bba15001f\thiserror-1.0.68\src\display.rs:
